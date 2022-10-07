90eb6e3a83c58b1d191fd2dc9de6a9af0efa27af
alexey1254 <alxkill54@gmail.com>
2022-10-07 18:06:46 +0100

    Se ven las cartas, falta por implementar la puntuacion

--- Tema03/librerias/funcionesAuxImagenes.php
+++ Tema03/librerias/funcionesAuxImagenes.php
@@ -1,8 +1,6 @@
 <?php 
 
     function mostrarNumImagen(string $numero, string $rutaRelativa, string $porcentaje="10") {
-        $numAleatorio=strval(random_int(10000,99999));
-
         for($i = 0; $i < strlen($numero); $i++) {
             echo ("<img src='$rutaRelativa/$numero[$i].png' alt='numero-$numero[$i]' width='{$porcentaje}%'/>\n");
         }
