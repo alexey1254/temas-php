60c244d3e74855d1c3861ecfe647a3b5d2405eba
vmcabreu <victormanueltoa@gmail.com>
2022-10-19 19:24:51 +0100

    Se hacen cosas

--- Tema04/signIn.php
+++ Tema04/signIn.php
@@ -1,24 +1,46 @@
+<?php 
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Cuenta</title>
 </head>
 <body>
-    <form>
-        <label>Nombre:</label>
-        <input type="text"/>
-        <label>Primer Apellido</label>
-        <input type="text"/>
-        <label>Segundo Apellido</label>
-        <input type="text"/>
-        <label>Nombre de Usuario</label>
-        <input type="text"/>
-        <label>Contraseña</label>
-        <input type="pass"/>
-        <label>E-mail</label>
-        <input type="email"/>
+    <style>
+        td {
+            text-align: right;
+        }
+    </style>
+    <form action="" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td><label>Nombre:</label></td>
+                <td><input type="text"/></td>
+            </tr>
+            <tr>
+                <td><label>Primer Apellido:</label></td>
+                <td><input type="text"/></td>
+            </tr>
+            <tr>
+                <td><label>Segundo Apellido:</label></td>
+                <td><input type="text"/></td>
+            </tr>
+            <tr>
+                <td><label>Nombre de Usuario:</label></td>
+                <td><input type="text"/><br/></td>
+            </tr>
+            <tr>
+                <td><label>Contraseña:</label></td>
+                <td><input type="password"/></td>
+            </tr>
+            <tr>
+                <td><label>E-mail:</label></td>
+                <td><input type="email"/><br/></td>
+            </tr>
+        </table>
         <button type="submit">Crear Cuenta</button>
     </form>
 </body>
